4bd9baef0334aeaf3cfda704d218041285f392c1
Maxim Kovalenko <ardash@mail.ru>
2013-10-23 18:26:21 +0400

    Initial Commit

---
+++ SRC/ExcelToXML_lib/libExcelToXML.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Xml;
+
+namespace Kovalenko.Classes {
+  public class libExcelToXML {
+    const string sRootEl = "root";
+    const string sItemEl = "item";
+
+    const string sId = "id";
+    const string sText = "text";
+
+    const string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                           "Data Source={0};" +
+                           "Extended Properties=\"Excel 12.0; HDR=NO\"";
+    const string encoding = "utf-8";
+
+    static XmlElement findById(XmlNode e, string id) {
+      XmlElement el;
+      if (e.Attributes != null && 
+          e.Attributes[sId] != null &&
+          e.Attributes[sId].Value == id) return e as XmlElement;
+
+      foreach (XmlNode n in e) if ((el = findById(n, id)) != null) return el;
+
+      return null;
+    }
+
+    // Преобразование Excel-XML
+    //  PathToExcelFile - имя Excel файла
+    //  PathToSaveFile - имя XML файла
+    //  errMsg - описание ошибки
+    // RC: false - ошибка
+    public static bool ExcelToXML(string PathToExcelFile, string PathToSaveFile, 
+                                 ref string errMsg) {
+      bool res = true;
+      try {
+        var s = String.Format(connStr, PathToExcelFile);
+        OleDbConnection conn = new OleDbConnection(s);
+        conn.Open();
+
+        var o = new object[] { null, null, null, "TABLE" };
+        DataTable sheets = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, o);
+        string shName = (string)sheets.Rows[0].ItemArray[2];
+        string cmdSelect = String.Format("SELECT * FROM [{0}]", shName);
+
+        OleDbDataAdapter a = new OleDbDataAdapter(cmdSelect, conn);
+        DataTable dt = new DataTable();
+        a.Fill(dt);
+        conn.Close();
+
+        XmlDocument XMLForSend = new XmlDocument();
+
+        // XML Declaration
+        XmlDeclaration xmlDecl;
+        xmlDecl = XMLForSend.CreateXmlDeclaration("1.0", encoding, null);
+        XMLForSend.InsertBefore(xmlDecl, XMLForSend.DocumentElement);
+
+        // Root
+        XmlElement rootEl = XMLForSend.CreateElement(sRootEl);
+        XMLForSend.AppendChild(rootEl);
+
+        // Elements
+        XmlElement tempRowNode;
+        foreach (DataRow curRow in dt.Rows) {
+          tempRowNode = XMLForSend.CreateElement(sItemEl);
+          tempRowNode.SetAttribute(sId, curRow.ItemArray[0].ToString());
+          tempRowNode.SetAttribute(sText, curRow.ItemArray[2].ToString());
+          XmlNode e = null;
+          var parent = curRow.ItemArray[1].ToString();
+          if (parent != "") e = findById(rootEl, parent);
+          if (e == null) e = rootEl;
+          e.AppendChild(tempRowNode);
+        }
+
+        XMLForSend.Save(PathToSaveFile);
+      } catch (Exception e) { res = false; errMsg = e.Message; }
+      return res;
+    }
+
+  }
+}
+
---
+++ SRC/TestExcelToXML/Program.cs
@@ -0,0 +1,41 @@
+﻿using Kovalenko.Classes;
+using System;
+
+namespace TestExcelToXML {
+  class Program {
+    const String cmdLineError = "Не заданы параметры\n"+
+                         "{0} <имя файла Excel> <имя XML файла для сохранения>";
+    const String sPressAnyKey = "Нажмите любую клавишу...";
+    const String processing = "Обрабатывается {0}";
+    const String sOK = "Обработка завершена";
+
+    static void pressAnyKey() {
+      print(sPressAnyKey);
+      Console.ReadKey();
+    }
+
+    static void print(String s) { 
+      Console.WriteLine(s); 
+    }
+
+    static void Main(string[] args) {
+      // Имя файла задано?
+      if (args.Length < 2 || args[0].Length <= 0 || args[1].Length <= 0) {
+        var exe = AppDomain.CurrentDomain.FriendlyName;
+        print(String.Format(cmdLineError, exe));
+        pressAnyKey();
+        return;
+      }
+
+      // Обработка файла
+      String ExcelName = args[0];
+      String XMLName = args[1];
+      print(String.Format(processing, ExcelName));
+      String err = "";
+      if (!libExcelToXML.ExcelToXML(ExcelName, XMLName, ref err)) print(err);
+      else print(sOK);
+
+      pressAnyKey();
+    }
+  }
+}
